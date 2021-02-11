ab00a34f0075262ab49b816f8b2f15a3efd28089
tineetinytin <christine.elene@gmail.com>
2021-02-11 11:20:49 -0800

    init commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+//STEP 1
+
+/*
+let someMonth;
+function theMonth();
+let currentMonth;
+let summerMonth;
+let myLibraryFunction;
+*/
+
+//STEP 2
+
+/*
+let num = 10; //numeric literal
+let num = prompt("Hello World!"); //string literal
+let num = false; //boolean
+let num = null; //null
+*/
+
+//STEP 3
+
+let  num = 4 + 9 - (2 / 4);
