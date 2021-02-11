1880f01477f3abbe9e8c89ef973094ab8e851ed4
tineetinytin <christine.elene@gmail.com>
2021-02-11 14:25:40 -0800

    finished all except 6 and 7

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//STEP 1
+//------- STEP 1
 
 /*
 let someMonth;
@@ -8,15 +8,152 @@ let summerMonth;
 let myLibraryFunction;
 */
 
-//STEP 2
+//------- STEP 2
 
-/*
-let num = 10; //numeric literal
-let num = prompt("Hello World!"); //string literal
-let num = false; //boolean
-let num = null; //null
-*/
 
-//STEP 3
+//  10              //numeric literal
+//  "Hello world"   //string literal
+//  false           //boolean literal
+//  null            //null literal
 
-let  num = 4 + 9 - (2 / 4);
+
+//------- STEP 3
+
+// let firstName = "Christine"; 
+// console.log((4 / 2) * 25 + 46);
+
+//------- STEP 4
+
+// let firstName;
+// let lastName;
+// let address;
+// let city;
+// let state;
+// let zip;
+// let age;
+// let referralSrc;
+// let contactYou;
+
+//------- STEP 5
+
+// let firstName;
+// let lastName;
+// let contactYou;
+
+// firstName = "Christine";
+// lastName = "Akers";
+// contactYou = true;
+// let first = prompt('What is your first name?');
+// let firstName = "Christine";
+// let lastName = "Akers";
+// let contactYou = true;
+
+// let firstName = "Christine", lastName = "Akers", contactYou = true;
+
+//------- STEP 6
+
+let num = prompt('Give me a number between 1 and 10');
+console.log(num);
+//------- STEP 7
+
+var contact = prompt('May we contact you?')
+var contact = true
+
+//------- STEP 8
+
+// let someString = `Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."`;
+// window.console.log(someString);
+
+//------- STEP 9
+
+// let contactYou2 = null;
+// console.log(contactYou2 + ',' + ' you may contact me');
+
+// let contactYou3;
+// console.log(contactYou3 + ',' + ' you may contact me');
+
+//------- STEP 10
+
+// let num = prompt('Give me a number between 1 and 10');
+// let num2 = 8;
+// let num3 = false;
+// let num4 = null;
+// let num5;
+
+// console.log('Num 1 ' + typeof num);
+// console.log('Num 2 ' + typeof num2);
+// console.log('Num 3 ' + typeof num3);
+// console.log('Num 4 ' + typeof num4);
+// console.log('Num 5 ' + typeof num5);
+
+//------- STEP 11
+
+// alert("Hello " + "Christine Akers" + " , welcome to the JavaScript class!");
+
+//------- STEP 12
+
+// let name = "Christine Akers";
+// alert("Hello " + name + " , welcome to the JavaScript class!");
+
+//------- STEP 13
+
+// let name = "christine Akers";
+// let course = "JavaScript";
+
+// alert("Hello " + name + ", welcome to the " + course + " class!");
+
+//------- STEP 14
+
+// let name = "Christine Akers";
+// let course = "JavaScript";
+
+// alert("Hello " + name + ", \nWelcome to the " + course + " class!");
+
+//------- STEP 15
+
+// let name = prompt('What is your name?');
+// let course = "JavaScript";
+
+// alert("Hello " + name + ", \nWelcome to the " + course + " class!");
+
+//------- STEP 16
+
+// let name = prompt('What is your name?');
+// let course = prompt('What class are you taking?');
+
+// alert("Hello " + name + ", \nWelcome to the " + course + " class!");
+
+//------- STEP 17
+
+// let x = 10;
+// let y = 20;
+
+// console.log(x + y); //30
+
+//------- STEP 18
+
+// let x = 20;
+// console.log(x += 20); //40
+
+//------- STEP 19
+
+// let x = 20;
+// console.log(x *= 5); //100
+
+//------- STEP 20
+
+// let x = 20 % 3;
+// console.log(x /= 1);
+
+//------- STEP 21
+
+// let x = 7;
+// let y = 3
+
+// console.log( x > y); //true
+
+//------- STEP 22
+
+// let x = 10;
+// let y = 8;
+// console.log( x == y ); //false
